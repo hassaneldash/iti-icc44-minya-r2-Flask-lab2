d39437badc6210df35eab5877cdd42df8d11b20c
hassaneldash <hassan.eldash@yahoo.com>
2024-03-02 17:00:06 +0200

    Final

--- file1.py
+++
@@ -1,9 +0,0 @@
-
-# File1.py  
-  
-print (f"File1 __name__ = {__name__}") #file1
-  
-if __name__ == "__main__":  
-    print ("File1 is being run directly") 
-else:  
-    print ("File1 is being imported") 
